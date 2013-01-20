0d28377da9ec283f5e713e2a23264a4571e6fa69
Shaked <shaked@shakedos.com>
2013-01-21 01:29:34 +0200

    added list viev using swing always on top is available

--- Gtask/src/main/java/com/gtask/main/Gtask.java
+++ Gtask/src/main/java/com/gtask/main/Gtask.java
@@ -1,5 +1,7 @@
 package com.gtask.main;
 
+import java.util.ArrayList;
+
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
@@ -7,11 +9,14 @@ import com.gtask.core.Task;
 import com.gtask.core.TaskList;
 import com.gtask.core.TasksLists;
 import com.gtask.services.Google;
+import com.gtask.services.Service;
 
-public class Gtask {
+public class Gtask { 
+	public static final Integer SERVICE_GOOGLE = 1;
+	
 	public static Logger log = Logger.getLogger(Gtask.class.getName());
 
-	public static void main(String args[]) {
+/*	public static void main(String args[]) {
 		// Set up a simple configuration that logs on the console.
 		BasicConfigurator.configure();
 
@@ -24,5 +29,32 @@ public class Gtask {
 			}
 		}
 		log.info("Done");
+	}*/
+	
+	public ArrayList<TasksLists> getTasksLists(){
+		//read config 
+		ArrayList<Integer> servicesIds = new ArrayList<Integer>();
+		servicesIds.add(new Integer(Gtask.SERVICE_GOOGLE));
+		
+		ArrayList<TasksLists> arrTasksLists = new ArrayList<TasksLists>();
+		for (Integer serviceId:servicesIds){
+			try { 
+				Service service = this.createService(serviceId);
+				TasksLists tasksLists = service.run(); 
+				arrTasksLists.add(tasksLists);
+			} catch(Exception e){
+				//TODO 
+			}
+		}
+		return arrTasksLists;
+	}
+	
+	private Service createService(Integer serviceId) throws Exception{
+		switch(serviceId){
+			case 1: 
+				return new Google(); 
+			default: 
+				throw new Exception("No such service");
+		} 
 	}
 }
